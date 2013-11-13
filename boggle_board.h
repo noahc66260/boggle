365fc450968d6701cff91b213b92af440c5eb185
Noah Ruderman <nruderman.s@gmail.com>
2013-11-12 17:54:24 -0800

    Included comments

--- boggle_board.h
+++ boggle_board.h
@@ -1,8 +1,12 @@
 // boggle_board.h
-// interface for BoggleBoard class
+// Interface for BoggleBoard class
+//
+// A boggle board is a 2D array of characters of dimensions rows() x cols().
+// The game of boggle is generally played with alphabetical characters but
+// any ASCII character may be used.
 
-#ifndef HOME_DOCUMENTS_FOO_BOGGLE_BOARD_H 
-#define HOME_DOCUMENTS_FOO_BOGGLE_BOARD_H
+#ifndef HOME_NOAH_DOCUMENTS_FOO_BOGGLE_BOARD_H 
+#define HOME_NOAH_DOCUMENTS_FOO_BOGGLE_BOARD_H
 
 #include <iostream>
 #include <vector>
@@ -18,15 +22,24 @@ class BoggleBoard
     ~BoggleBoard() {};
     int rows() const {return rows_;}
     int cols() const {return cols_;}
-    // this function should be readFromStream(), not readFromFile()
-    bool readFromStream(istream& is); // does not close file
-    // should i bother overloading [][]?
+
+    // Uses std::cin object to read in rows() x cols() characters into
+    //  the board which can be retrieved with the at() function. 
+    // The characters are stored in row major order. 
+    // Returns true if rows() x cols() characters were read in successfully.
+    bool readFromStream(istream& is); 
+
+    // Returns true if we have rows() x cols() characters stored in the 
+    //  board and each character is alphabetical. 
+    bool isValid(); 
+
+    // Returns the character stored at index (i, j) in the board.
     char& at(int i, int j);
-    const char& at(int i, int j) const; // the need for this confuses me
+    const char& at(int i, int j) const; 
   private:
     int rows_, cols_;
     vector< vector<char> > board_;
 };
 
 
-#endif //HOME_DOCUMENTS_FOO_BOGGLE_BOARD_H
+#endif // HOME_NOAH_DOCUMENTS_FOO_BOGGLE_BOARD_H
