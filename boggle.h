818d8c978d0733f16a53559ddd506290dba8c322
Noah Ruderman <nruderman.s@gmail.com>
2013-11-30 01:10:30 -0500

    Created unnamed namespace for buffer_len variable

--- boggle.h
+++ boggle.h
@@ -1,18 +1,33 @@
 // boggle.h
-// Prototypes for functions to read in and solve boggle games
+//
+// Functions to parse command-line input for specifying boggle board
+//  dimensions and which file to use as a dictionary.
 
 #ifndef HOME_NOAH_DOCUMENTS_FOO_BOGGLE_H
 #define HOME_NOAH_DOCUMENTS_FOO_BOGGLE_H
 
-const int buffer_len = 500; // how do i deal with this?
+// If an option takes an argument, we'll need to store its argument. To
+//  avoid using dynamic memory allocation, we store values in buffers of
+//  fixed size. The choice of 500 is somewhat arbitrary and may be changed
+//  if there is good reason.
+// An unnamed namespace is used in accordance with coding standards
+namespace {
 
-void parseOptions(int argc, char **argv, char cvalue[buffer_len], 
-                  char rvalue[buffer_len], char dvalue[buffer_len]);
+const int buffer_len = 500; 
 
-void setValues(char cvalue[buffer_len], char rvalue[buffer_len],
-               char dvalue[buffer_len], int* prows, int* pcols, 
-               char pfile[buffer_len]);
+}
 
+// When called, prows, pcols, and pfile will be modified to reflect
+//  the specifications as dictated by the argv string. Uses getops()
+//  function to process the options. The options are as follows:
+//
+//  -r num  Specifies the number of rows. Must be used with -c option.
+//  -c num  Specifies the number of columns. Must be used with -r option.
+//  -d pathname   Specifies the pathname of the dictionary to be used.
+//
+//  The -r and -c options must be used together, or not at all. The default
+//    values (if neither -r nor -c is used) are 4 and 4.
+//  The -d option is optional whose default value is /usr/dict/share/words
 void processOptions(int argc, char **argv, int* prows, int* pcols, 
                     char pfile[buffer_len]);
 
